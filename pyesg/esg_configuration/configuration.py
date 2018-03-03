bcc3a13b0d82e2fc416ea80e9d263945dd931207
Rajiv Patel <rajivpatel36@gmail.com>
2018-03-03 22:27:59 +0000

    Added classes to use for the configuration.

---
+++ pyesg/esg_configuration/configuration.py
@@ -0,0 +1,297 @@
+import json
+
+from collections import OrderedDict
+from typing import List, Dict
+
+
+def _has_parameters(Cls):
+    """
+    A decorator for any class which has parameters which creates an add_parameter method for the class.
+    Args:
+        Cls: The class with parameters
+
+    Returns:
+        An augmented class version of the original class which has an add_parameter method.
+
+    """
+    class AugmentedCls(Cls):
+        def add_parameter(self, id: str, value: str):
+            parameter = Parameter(id=id, value=value)
+            self.parameters.append(parameter)
+    return AugmentedCls
+
+
+class JSONSerialisableClass:
+    """
+    Base class for encoding and decoding JSON objects representing the pyESG configuration.
+    """
+    _serialisable_lists = {}  # type: Dict[str, JSONSerialisableClass]
+    _serialisable_attrs = {}  # type: Dict[str, JSONSerialisableClass]
+
+    def __init__(self, **kwargs):
+        self.__dict__.update(kwargs)
+
+    def __repr__(self):
+        return json.dumps(self._encode_json(), indent=4)
+
+    def _encode_json(self)->dict:
+        json_obj = {}
+        for key, value in self.__dict__.items():
+            if isinstance(value, JSONSerialisableClass):
+                json_obj[key] = value._encode_json()
+            elif isinstance(value, list) and len(value) > 0 and isinstance(value[0], JSONSerialisableClass):
+                json_obj[key] = [item._encode_json() for item in value]
+            else:
+                json_obj[key] = value
+        return OrderedDict(sorted(json_obj.items()))
+
+    @classmethod
+    def _decode_json(cls, json_obj: dict):
+        instance = cls()
+        for key, value in json_obj.items():
+            if key in cls._serialisable_lists:
+                class_type = cls._serialisable_lists[key]
+                set_value = [class_type._decode_json(item) for item in value]
+            elif key in cls._serialisable_attrs:
+                class_type = cls._serialisable_attrs[key]
+                set_value = class_type._decode_json(value)
+            else:
+                set_value = value
+            instance.__setattr__(key, set_value)
+        return instance
+
+
+class Parameter(JSONSerialisableClass):
+    """
+    Represents a parameter in the pyESG configuration.
+    Attributes:
+        id (str): The id (or name) for the parameter.
+        value (float): The parameter value.
+    """
+    def __init__(self, **kwargs):
+        self.id = None  # type: str
+        self.value = None  # type: float
+        super().__init__(**kwargs)
+
+
+@_has_parameters
+class Output(JSONSerialisableClass):
+    """
+    Represents a model output in the pyESG configuration.
+    Attributes:
+        id (str): The id (or name) for the output.
+        type (str): The type of the output.
+        initial_value (float): The initial value for the output.
+        parameters (List[Parameter]): A list of the parameters associated with the output.
+    """
+    _serialisable_lists = {
+        'parameters': Parameter,
+    }
+
+    def __init__(self, **kwargs):
+        self.id = None  # type: str
+        self.type = None  # type: str
+        self.initial_value = None  # type: float
+        self.parameters = []  # type: List[Parameter]
+        super().__init__(**kwargs)
+
+
+@_has_parameters
+class RandomDriver(JSONSerialisableClass):
+    """
+    Represents a random driver for a model in the pyESG configuration.
+    Attributes:
+        id (str): The id (or name) for the random driver.
+        distribution (str): The underlying statistical distribution from which samples for this driver come from.
+        parameters (List[Parameter]): A list of the parameters associated with the distribution for the random driver.
+    """
+    _serialisable_lists = {
+        'parameters': Parameter,
+    }
+
+    def __init__(self, **kwargs):
+        self.id = None  # type: str
+        self.distribution = None  # type: str
+        self.parameters = []  # type: List[Parameter]
+        super().__init__(**kwargs)
+
+
+@_has_parameters
+class AssetClass(JSONSerialisableClass):
+    """
+    Represents an asset class being modelled in the pyESG configuration.
+    Attributes:
+        id (str): The id (or name) for the asset class.
+        model_id (str): The id (or name) of the model being used for the asset class.
+        parameters (List[Parameter]): A list of the parameters associated with the model.
+        outputs (List[Output]): A list of the outputs to be calculated for the model.
+        random_drivers (List[RandomDriver]): A list of the random drivers associated with the model.
+        dependencies (List[str]): A list of the ids of other asset classes upon which this asset class depends.
+    """
+    _serialisable_lists = {
+        'parameters': Parameter,
+        'outputs': Output,
+        'random_drivers': RandomDriver,
+    }
+
+    def __init__(self, **kwargs):
+        self.id = None  # type: str
+        self.model_id = None  # type: str
+        self.parameters = []  # type: List[Parameter]
+        self.outputs = []   # type: List[Output]
+        self.random_drivers = []  # type: List[RandomDriver]
+        self.dependencies = []  # type: List[str]
+        super().__init__(**kwargs)
+
+    def add_output(self, id: str, type: str, initial_value: float=None, **kwargs):
+        """
+        Adds an output to the list of outputs for the asset class.
+        Args:
+            id: The id (or name) for the output.
+            type: The type of the output.
+            initial_value: The initial value of the output.
+            **kwargs: The parameters for the output. The keywords are the parameter ids and the values are the parameter
+                      values.
+        """
+        output = Output(id=id, type=type, initial_value=initial_value)
+        for key, value in kwargs.items():
+            output.add_parameter(key, value)
+        self.outputs.append(output)
+
+
+class Correlations(JSONSerialisableClass):
+    """
+    Represents the correlation matrix between the random drivers in the pyESG configuration.
+    """
+    def __init__(self, **kwargs):
+        self._correlations = OrderedDict()
+        super().__init__(**kwargs)
+
+    def get_correlation(self, row_id: str, column_id: str)->float:
+        """
+        Returns an entry from the correlation matrix based on the row and column ids.
+        Args:
+            row_id: The id for the row.
+            column_id: The id for the column.
+
+        Returns:
+            The correlation associated with the specified row and column id.
+            If the two ids are equal, 1 is returned. If no correlation has been specified for this pair of ids,
+            0 is returned.
+        """
+        min_id = min(row_id, column_id)
+        max_id = max(row_id, column_id)
+
+        if min_id == max_id:
+            return 1.0
+        else:
+            return self._correlations.get((min_id, max_id), default=0.0)
+
+    def set_correlation(self, row_id: str, column_id: str, correlation: float):
+        """
+        Sets the value for an entry in the correlation matrix based on the row and column ids.
+        Args:
+            row_id: The id for the row.
+            column_id: The id for the column.
+            correlation: The correlation associated with the specified row and column id.
+                         This value is ignored if the row and column ids are equal.
+        """
+        min_id = min(row_id, column_id)
+        max_id = max(row_id, column_id)
+
+        if min_id != max_id:
+            self._correlations[(min_id, max_id)] = correlation
+
+    def _encode_json(self):
+        encoded_json = []
+        for key, value in self._correlations.items():
+            encoded_json.append({
+                'row_id': key[0],
+                'column_id': key[1],
+                'correlation': value,
+            })
+
+        return encoded_json
+
+    @classmethod
+    def _decode_json(cls, json_obj: dict):
+        instance = cls()
+        for correlation in json_obj:
+            instance.set_correlation(correlation['row_id'], correlation['column_id'], correlation['correlation'])
+
+        return instance
+
+
+class Economy(JSONSerialisableClass):
+    """
+    Represents an economy for which asset classes are being modelled in the pyESG configuration.
+    Attributes:
+        id (str): The id (or name) of the economy.
+        asset_classes (List[AssetClass]): A list of the asset classes being modelled in the economy.
+    """
+    _serialisable_lists = {
+        'asset_classes': AssetClass,
+    }
+
+    def __init__(self, **kwargs):
+        self.id = None  # type: str
+        self.asset_classes = []  # type: List[AssetClass]
+        super().__init__(**kwargs)
+
+
+class PyESGConfiguration(JSONSerialisableClass):
+    """
+    Represents the full pyESG configuration.
+    Attributes:
+        number_of_simulations (int): The number of simulations to produce.
+        number_of_projection_steps (int): The number of time steps to project in the simulations.
+        time_step_length (float): The length of each time step, in years.
+        number_of_batches (int): The number of batches into which the simulations are split during generation.
+        random_seed (int): The random seed to use when generating random samples.
+        economies (list[Economy]): A list of the economies being modelled.
+        correlations (Correlations): The correlations between the random drivers for the asset class models.
+    """
+    _serialisable_lists = {
+        'economies': Economy,
+    }
+
+    _serialisable_attrs = {
+        'correlations': Correlations,
+    }
+
+    def __init__(self, **kwargs):
+        self.number_of_simulations = None  # type: int
+        self.number_of_projection_steps = None  # type: int
+        self.output_file_directory = None  # type: str
+        self.output_file_name = None  # type: str
+        self.time_step_length = None  # type: float
+        self.number_of_batches = None  # type: int
+        self.random_seed = None  # type: int
+        self.economies = []  # type: List[Economy]
+        self.correlations = Correlations()  # type: Correlations
+        super().__init__(**kwargs)
+
+    @classmethod
+    def load_from_file(cls, file_path: str) -> 'PyESGConfiguration':
+        """
+        Loads settings from a pyESG configuration file.
+        Args:
+            file_path: The file path of the pyESG configuration file.
+
+        Returns:
+            A PyESGConfiguration object containing the settings from the specified configuration file.
+        """
+        with open(file_path) as config_file:
+            config = json.load(config_file)
+
+        pyesg_config = cls._decode_json(config)  # type: 'PyESGConfiguration'
+        return pyesg_config
+
+    def save_to_file(self, file_path: str):
+        """
+        Saves the configuration settings specified to a file.
+        Args:
+            file_path: The file path to which to save the configuration file.
+        """
+        with open(file_path, 'w') as engine_settings_file:
+            json.dump(self._encode_json(), engine_settings_file, indent=4)
